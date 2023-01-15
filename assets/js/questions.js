90cbe3b949c87be6bda2958d40ee8d622ee72dd7
annabessant <anna@thefutureofinnovation.org>
2023-01-15 23:37:33 +0000

    add questions to question.js file

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,5 +1,25 @@
 var questions = [
-    {
-        question:"Where is the correct place to inser a JavaScript?"
-    }
-]
+    {question:"Where is the correct place to inser a JavaScript?",
+    choices: ["The <head> section", "The <body> section","Both the <head> and the <body> sections are correct", "After the <body> section"],
+    answer: "Both the <head> and the <body> sections are correct"},
+
+    {question:"Inside which HTML element do we put the JavaScript?",
+    choices: ["<javascript>","<script>","<js>","<scripfile>"],
+    answer:"<script>"},
+
+    {question:"How to create "Hello World" alert box?",
+    choices:["alert('Hello World')","msgBox('Hello World')","msg('Hello World')","alertBox('Hello World')"],
+    answer: "alert('Hello World')"},
+
+    {question:"The condition in an if / else statement is enclosed with?",
+    choices:["quotes","curly brakets","parenthesis","square brakets"],
+    answer:"parenthesis"},
+
+    {question:"Arrays in JavaSCript can be used to store:",
+    choices:["numbers and strings","other arrays","booleans","all of the above"],
+    answer:"all of the above"},
+
+    {question:"When being assigned to variable, string values must be enclosed within:",
+    choices:["commas","curly brakets","quaotes","parenthesis"],
+    answer:"curly brakets"},
+];
