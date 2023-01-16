681564eace4916e569ff7ee3af450beb99d3e93b
annabessant <anna@thefutureofinnovation.org>
2023-01-16 00:15:11 +0000

    add added getElementById elements into logic.js file

---
+++ assets/js/logic.js
@@ -0,0 +1,11 @@
+//Get title, form, start-quiz, feedback, timer, reset, submit-score, clear-score, header-text, view-score elements:
+var title = document.getElementById('title');
+var form = document.getElementById('form');
+var startButton = document.getElementById('start-quiz');
+var feedback = document.getElementById('feedback');
+var timerDisplay = document.getElementById('timer');
+var resetButton = document.getElementById('reset');
+var submitScoreButton = document.getElementById('submit-score');
+var clearScoreButton = document.getElementById('clear-score');
+var headerText = document.getElementById("header-text");
+var viewScore = document.getElementById("view-score");
