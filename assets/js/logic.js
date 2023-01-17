2ca67fe411708948f71796fc2fde692785d339b8
annabessant <anna@thefutureofinnovation.org>
2023-01-17 20:26:16 +0000

    add fixed errors

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,8 +2,10 @@
 var title = document.getElementById('question-title');
 var form = document.getElementById('choices');
 var startButton = document.getElementById('start');
+var questionDiv = document.getElementById('questions'); 
 var feedback = document.getElementById('feedback');
 var timerDisplay = document.getElementById('timer');
+var timerEl = document.getElementById('time');
 // var resetButton = document.getElementById('reset');
 var submitScoreButton = document.getElementById('submit');
 // var clearScoresButton = document.getElementById('clear-scores');
@@ -14,18 +16,34 @@ var viewScores = document.getElementById("view-scores");
 // userInfo.name = window.prompt("Please leave your name", "Peppa Pig");
 
 // Add event listeners: display the form, start timer, collect data from the form
+var timerId;
+var timeRemaining = 120;
+
 startButton.addEventListener('click', function() {
+    console.log ("startButtonClick");
+    var startDiv=document.getElementById ("start-screen");
+    startDiv.setAttribute ("class", "hide"); 
+    questionDiv.removeAttribute('class');
     counter2 = 0;
-    title.textContent = "JavaScript Codimng Quiz";
+    title.textContent = "JavaScript Coding Quiz";
     // headerText.textContent = "Welcome to our time Quiz! Your score is linked to the time left on the timer. You have 6 questions to answer within 60 seconds. For each incorrect answer you loose 10 seconds. BEST of LUCK!"
     updateQuestion();
     // viewScores.style.display = "block";
     // timerDisplay.style.display = "block";
     startButton.style.display = "none";
     form.style.display = "block";
-    userInfo.score = startTimer(time, timerDisplay);
+    // userInfo.score = startTimer(time, timerDisplay);
+    timerEl.textContent = timeRemaining;
+    timerId = setInterval (timerFunc, 1000);
 });
 
+function timerFunc(){
+    timeRemaining--;
+    timerEl.textContent = timeRemaining;
+    if (timeRemaining <=0){
+        //tbc
+    }
+}
 //Collect data from the form
 form.addEventListener('submit', function(event){
     event.preventDefault();
